dea67b323bcd4c143ddeea34290e94930dd1882e
Ashwin Naren <arihant2math@gmail.com>
2023-05-13 21:52:38 -0700

    tiles

--- mapgen/create_map.py
+++ mapgen/create_map.py
@@ -6,29 +6,33 @@ from PIL import Image, ImageDraw
 
 data = pickle.load(open("data.pickle", "rb"))
 
-fnt = ImageFont.truetype('arial.ttf', 30)
+fnt = ImageFont.truetype('arial.ttf', 65)
 # Config
-width = 80000
-height = 80000
-system_size = 140
+tile_x = 10000
+tile_y = 10000
+system_size = 300
+tiles = 5
 
 print("Mapgen")
 print("----------------")
 print("Total Systems: " + str(len(data)))
-print("Width: " + str(width))
-print("Height: " + str(height))
+print("Width: " + str(tile_x))
+print("Height: " + str(tile_y))
 print("System Size: " + str(system_size))
+print("Tiles: " + str(tiles**2))
 print("Creating blank image")
-img = Image.new('RGB', (width, height), color='black')
+img = Image.new('RGB', (tile_x, tile_y), color='black')
 print("Creating ImageDraw instance")
 d = ImageDraw.Draw(img)
-print("Drawing ...")
-for system in tqdm(data):
-    image_x = (system.x*system_size) + width//2
-    image_y = (system.y*system_size) + height//2
-    d.ellipse((
-        (image_x-system_size//2, image_y-system_size//2), (image_x+system_size//2, image_y+system_size//2)),
-              fill=(46, 46, 46), outline="white")
-    d.text((image_x, image_y), system.symbol, fill="blue", font=fnt, anchor="ms")
-print("Writing ...")
-img.save('image.png')
+for x_tile in range(tiles):
+    for y_tile in range(tiles):
+        print("Drawing tile " + str(x_tile) + ", " + str(y_tile))
+        for system in data:
+            image_x = (system.x*system_size) + tile_x//2 - x_tile*tile_x
+            image_y = (system.y*system_size) + tile_y//2 - y_tile*tile_y
+            d.ellipse((
+                (image_x-system_size//2, image_y-system_size//2), (image_x+system_size//2, image_y+system_size//2)),
+                fill=(46, 46, 46), outline="white")
+            d.text((image_x, image_y), system.symbol, fill="blue", font=fnt, anchor="ms")
+        print("Writing ...")
+        img.save("tiles/tile_" + str(x_tile) + "_" + str(y_tile) + ".png")

efa90a27e0ad8f20b492dda499d9b52b0fec161c
Ashwin Naren <arihant2math@gmail.com>
2023-05-14 11:38:44 -0700

    added map

--- mapgen/create_map.py
+++ mapgen/create_map.py
@@ -11,7 +11,9 @@ fnt = ImageFont.truetype('arial.ttf', 65)
 tile_x = 10000
 tile_y = 10000
 system_size = 300
-tiles = 5
+tiles = 8
+total_x = tile_x*tiles
+total_y = tile_y*tiles
 
 print("Mapgen")
 print("----------------")
@@ -20,16 +22,14 @@ print("Width: " + str(tile_x))
 print("Height: " + str(tile_y))
 print("System Size: " + str(system_size))
 print("Tiles: " + str(tiles**2))
-print("Creating blank image")
-img = Image.new('RGB', (tile_x, tile_y), color='black')
-print("Creating ImageDraw instance")
-d = ImageDraw.Draw(img)
 for x_tile in range(tiles):
     for y_tile in range(tiles):
         print("Drawing tile " + str(x_tile) + ", " + str(y_tile))
+        img = Image.new('RGB', (tile_x, tile_y), color='black')
+        d = ImageDraw.Draw(img)
         for system in data:
-            image_x = (system.x*system_size) + tile_x//2 - x_tile*tile_x
-            image_y = (system.y*system_size) + tile_y//2 - y_tile*tile_y
+            image_x = (system.x*system_size) + (total_x//2) - x_tile*tile_x
+            image_y = (system.y*system_size) + (total_y//2) - y_tile*tile_y
             d.ellipse((
                 (image_x-system_size//2, image_y-system_size//2), (image_x+system_size//2, image_y+system_size//2)),
                 fill=(46, 46, 46), outline="white")
--- website/bp/main.py
+++ website/bp/main.py
@@ -1,3 +1,8 @@
+import os
+from io import BytesIO
+from pathlib import Path
+
+from PIL import Image
 from autotraders.agent import Agent
 from flask import *
 
@@ -18,6 +23,47 @@ def map():
     return render_template("map.html")
 
 
-@main_bp.route("/map/tile/{z}/{x}/{y}.png")
-def map_api(z, x, y):
-    abort(404)
+@main_bp.route("/map_tile/<z>/<x>/<y>.png")
+async def map_api(z, x, y):
+    x = int(x)
+    y = int(y)
+    z = int(z)
+    tiles_path = Path(os.getcwd()) / "website" / "static" / "tiles"
+    if z == 2:
+        img = Image.new('RGBA', (10000, 10000), color='black')
+        image1 = Image.open(tiles_path / ("tile_" + str(x) + "_" + str(y) + ".png")).reduce(2)
+        image2 = Image.open(tiles_path / ("tile_" + str(x+1) + "_" + str(y) + ".png")).reduce(2)
+        image3 = Image.open(tiles_path / ("tile_" + str(x) + "_" + str(y+1) + ".png")).reduce(2)
+        image4 = Image.open(tiles_path / ("tile_" + str(x+1) + "_" + str(y+1) + ".png")).reduce(2)
+        img.paste(image1, (0, 0, 5000, 5000))
+        img.paste(image2, (5000, 0, 10000, 5000))
+        img.paste(image3, (0, 5000, 5000, 10000))
+        img.paste(image4, (5000, 5000, 10000, 10000))
+        new_img = img.reduce(8)
+        img_io = BytesIO()
+        new_img.save(img_io, 'PNG')
+        img_io.seek(0)
+        return send_file(img_io, mimetype='image/png')
+    elif z == 3:
+        img = Image.open(
+            tiles_path / ("tile_" + str(int(x)) + "_" + str(int(y)) + ".png"))
+        new_img = img.reduce(8)
+        img_io = BytesIO()
+        new_img.save(img_io, 'PNG')
+        img_io.seek(0)
+        return send_file(img_io, mimetype='image/png')
+    else:
+
+        img = Image.open(tiles_path / ("tile_" + str(int(8*x/(2**z))) + "_" + str(int(8*y/(2**z))) + ".png"))
+        x_part = 8*x/(2**z) % 1
+        y_part = 8*y/(2**z) % 1
+        assert img.height == img.width
+        dimension = img.height
+        scale = int(8*(1/z**2)*dimension)
+        x_coord = int(x_part*dimension)
+        y_coord = int(y_part*dimension)
+        new_img = img.crop((x_coord, y_coord, x_coord + scale, y_coord + scale))
+        img_io = BytesIO()
+        new_img.save(img_io, 'PNG')
+        img_io.seek(0)
+        return send_file(img_io, mimetype='image/png')

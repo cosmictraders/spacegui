9c99095eba94616fa9d84a3e4d70dbfdb3fb0678
Ashwin Naren <arihant2math@gmail.com>
2023-05-16 21:23:41 -0700

    added ship error handling

--- website/frontend/webpack.config.js
+++ website/frontend/webpack.config.js
@@ -1,13 +1,29 @@
-const {resolve} = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
     mode: 'development',
     entry: {
-        'main': './src/main.js',
+        'main': './src/js/main.js',
+        'canvas': './src/js/map.js'
     },
     output: {
-        path: resolve(__dirname, "..", 'static'),
+        path: path.resolve(__dirname, "..", 'static'),
         filename: '[name].js'
-    }
-}
+    },
+    plugins: [
+        new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, ".")
+        }),
+        // Have this example work in Edge which doesn't ship `TextEncoder` or
+        // `TextDecoder` at this time.
+        new webpack.ProvidePlugin({
+          TextDecoder: ['text-encoding', 'TextDecoder'],
+          TextEncoder: ['text-encoding', 'TextEncoder']
+        })
+    ],
+    experiments: {
+        asyncWebAssembly: true
+   }
+};
--- website/static/main.js
+++ website/static/main.js
@@ -9,24 +9,24 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/main.js":
-/*!*********************!*\
-  !*** ./src/main.js ***!
-  \*********************/
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _theme__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./theme */ \"./src/theme.js\");\n/* harmony import */ var _theme__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_theme__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\n\n//# sourceURL=webpack://frontend/./src/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _theme__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./theme */ \"./src/js/theme.js\");\n/* harmony import */ var _theme__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_theme__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\n\n//# sourceURL=webpack://frontend/./src/js/main.js?");
 
 /***/ }),
 
-/***/ "./src/theme.js":
-/*!**********************!*\
-  !*** ./src/theme.js ***!
-  \**********************/
+/***/ "./src/js/theme.js":
+/*!*************************!*\
+  !*** ./src/js/theme.js ***!
+  \*************************/
 /***/ (() => {
 
-eval("(() => {\r\n    'use strict'\r\n\r\n    const storedTheme = localStorage.getItem('theme')\r\n\r\n    const getPreferredTheme = () => {\r\n        if (storedTheme) {\r\n            return storedTheme\r\n        }\r\n\r\n        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'\r\n    }\r\n\r\n    const setTheme = function (theme) {\r\n        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {\r\n            document.documentElement.setAttribute('data-bs-theme', 'dark')\r\n        } else {\r\n            document.documentElement.setAttribute('data-bs-theme', theme)\r\n        }\r\n    }\r\n\r\n    setTheme(getPreferredTheme())\r\n\r\n    const showActiveTheme = (theme, focus = false) => {\r\n        const themeSwitcher = document.querySelector('#bd-theme')\r\n\r\n        if (!themeSwitcher) {\r\n            return\r\n        }\r\n\r\n        const themeSwitcherText = document.querySelector('#bd-theme-text')\r\n        const activeThemeIcon = document.querySelector('.theme-icon-active use')\r\n        const btnToActive = document.querySelector(`[data-bs-theme-value=\"${theme}\"]`)\r\n        const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')\r\n\r\n        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {\r\n            element.classList.remove('active')\r\n            element.setAttribute('aria-pressed', 'false')\r\n        })\r\n\r\n        btnToActive.classList.add('active')\r\n        btnToActive.setAttribute('aria-pressed', 'true')\r\n        activeThemeIcon.setAttribute('href', svgOfActiveBtn)\r\n        const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`\r\n        themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)\r\n\r\n        if (focus) {\r\n            themeSwitcher.focus()\r\n        }\r\n    }\r\n\r\n    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {\r\n        if (storedTheme !== 'light' || storedTheme !== 'dark') {\r\n            setTheme(getPreferredTheme())\r\n        }\r\n    })\r\n\r\n    window.addEventListener('DOMContentLoaded', () => {\r\n        showActiveTheme(getPreferredTheme())\r\n\r\n        document.querySelectorAll('[data-bs-theme-value]')\r\n            .forEach(toggle => {\r\n                toggle.addEventListener('click', () => {\r\n                    const theme = toggle.getAttribute('data-bs-theme-value')\r\n                    localStorage.setItem('theme', theme)\r\n                    setTheme(theme)\r\n                    showActiveTheme(theme, true)\r\n                })\r\n            })\r\n    })\r\n})()\r\n\n\n//# sourceURL=webpack://frontend/./src/theme.js?");
+eval("(() => {\r\n    'use strict'\r\n\r\n    const storedTheme = localStorage.getItem('theme')\r\n\r\n    const getPreferredTheme = () => {\r\n        if (storedTheme) {\r\n            return storedTheme\r\n        }\r\n\r\n        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'\r\n    }\r\n\r\n    const setTheme = function (theme) {\r\n        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {\r\n            document.documentElement.setAttribute('data-bs-theme', 'dark')\r\n        } else {\r\n            document.documentElement.setAttribute('data-bs-theme', theme)\r\n        }\r\n    }\r\n\r\n    setTheme(getPreferredTheme())\r\n\r\n    const showActiveTheme = (theme, focus = false) => {\r\n        const themeSwitcher = document.querySelector('#bd-theme')\r\n\r\n        if (!themeSwitcher) {\r\n            return\r\n        }\r\n\r\n        const themeSwitcherText = document.querySelector('#bd-theme-text')\r\n        const activeThemeIcon = document.querySelector('.theme-icon-active use')\r\n        const btnToActive = document.querySelector(`[data-bs-theme-value=\"${theme}\"]`)\r\n        const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')\r\n\r\n        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {\r\n            element.classList.remove('active')\r\n            element.setAttribute('aria-pressed', 'false')\r\n        })\r\n\r\n        btnToActive.classList.add('active')\r\n        btnToActive.setAttribute('aria-pressed', 'true')\r\n        activeThemeIcon.setAttribute('href', svgOfActiveBtn)\r\n        const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`\r\n        themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)\r\n\r\n        if (focus) {\r\n            themeSwitcher.focus()\r\n        }\r\n    }\r\n\r\n    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {\r\n        if (storedTheme !== 'light' || storedTheme !== 'dark') {\r\n            setTheme(getPreferredTheme())\r\n        }\r\n    })\r\n\r\n    window.addEventListener('DOMContentLoaded', () => {\r\n        showActiveTheme(getPreferredTheme())\r\n\r\n        document.querySelectorAll('[data-bs-theme-value]')\r\n            .forEach(toggle => {\r\n                toggle.addEventListener('click', () => {\r\n                    const theme = toggle.getAttribute('data-bs-theme-value')\r\n                    localStorage.setItem('theme', theme)\r\n                    setTheme(theme)\r\n                    showActiveTheme(theme, true)\r\n                })\r\n            })\r\n    })\r\n})()\r\n\n\n//# sourceURL=webpack://frontend/./src/js/theme.js?");
 
 /***/ })
 
@@ -102,7 +102,7 @@ eval("(() => {\r\n    'use strict'\r\n\r\n    const storedTheme = localStorage.g
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/main.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/main.js");
 /******/ 	
 /******/ })()
 ;

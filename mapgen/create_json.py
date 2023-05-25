ce6379b9c159933b3acfbdcb2efd9778273ed29a
Ashwin Naren <arihant2math@gmail.com>
2023-05-25 12:12:53 -0700

    map update

--- mapgen/create_json.py
+++ mapgen/create_json.py
@@ -1,13 +1,24 @@
 import json
 import pickle
+import random
 
-data = pickle.load(open("data.pickle", "rb"))
+from autotraders.map.system import System
+
+data: list[System] = pickle.load(open("data.pickle", "rb"))
 data_dict = {}
 for i in data:  # TODO: Waypoints
-    data_dict[i.symbol] = {
+    waypoints = {}
+    for w in i.waypoints:
+        traits = []
+        for trait in w.traits:
+            traits.append(trait.symbol)
+        waypoints[str(w.symbol)] = {"x": w.x, "y": w.y, "traits": traits, "type": w.waypoint_type}
+    data_dict[str(i.symbol)] = {
         "type": i.star_type,
         "x": i.x,
         "y": i.y,
         "factions": i.factions,
+        "waypoints": []
     }
+
 print(json.dumps(data_dict, indent=4))

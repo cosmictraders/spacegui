12f7f7cf087c7c1e87a7d41e411ef832c98f2053
Ashwin Naren <arihant2math@gmail.com>
2023-06-29 17:53:40 -0700

    added version check

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import autotraders
+
 from website.app import create_app
 
-# TODO: Implement autotraders version check
+assert autotraders.__version__.split('.')[0] == "1"
+assert autotraders.__version__.split('.')[1] == "6"
 
 if __name__ == "__main__":
     create_app().run(debug=True)

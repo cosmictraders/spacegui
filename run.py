a0e3d7222ca68e5dab5c3e8d36d87f34411b9e4d
Ashwin Naren <arihant2math@gmail.com>
2023-07-13 17:50:13 -0700

    latest autotraders support

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import autotraders
 from website.app import create_app
 
 assert autotraders.__version__.split(".")[0] == "1"
-assert autotraders.__version__.split(".")[1] == "6"
+assert autotraders.__version__.split(".")[1] == "7"
 
 if __name__ == "__main__":
     create_app().run(debug=True)

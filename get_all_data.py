191b7a26775e556863c65ebff3064bfc1cc00aee
Ashwin Naren <arihant2math@gmail.com>
2023-08-17 21:56:17 -0700

    autocomplete and search update

--- get_all_data.py
+++ get_all_data.py
@@ -11,8 +11,15 @@ from secret import TOKEN
 s = get_session(TOKEN)
 all_factions = Faction.all(s)
 pickle.dump(all_factions[1], open("factions.pickle", "wb"))
-all_systems = System.all(s)
-for i in tqdm(range(1, all_systems.pages + 1)):
-    all_systems.next()
-print("Writing ...")
-pickle.dump(all_systems.stitch(), open("data.pickle", "wb"))
+try:
+    all_systems = []
+    data = s.get(s.base_url + "systems.json").json()
+    for jsys in data:
+        all_systems.append(System(jsys["symbol"], s, jsys))
+    pickle.dump(all_systems, open("data.pickle", "wb"))
+except:
+    all_systems = System.all(s)
+    for i in tqdm(range(1, all_systems.pages + 1)):
+        all_systems.next()
+    print("Writing ...")
+    pickle.dump(all_systems.stitch(), open("data.pickle", "wb"))

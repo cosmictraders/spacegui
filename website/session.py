04e82b7664a281ed5a6be0c324563c08e63ecf40
Ashwin Naren <arihant2math@gmail.com>
2023-10-24 12:47:28 -0700

    formatting

--- website/session.py
+++ website/session.py
@@ -7,6 +7,7 @@ def get_session():
     user = db.session.query(User).filter_by(active=True).first()
     return session.AutoTradersSession(user.token)
 
+
 def get_token():
     user = db.session.query(User).filter_by(active=True).first()
     return user.token

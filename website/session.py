4e789b5387feaabc089704b652174c16c760d795
Ashwin Naren <arihant2math@gmail.com>
2023-10-26 15:47:34 -0700

    automatically make the first user active if none are

--- website/session.py
+++ website/session.py
@@ -5,6 +5,10 @@ from website.model import db, User
 
 def get_session():
     user = db.session.query(User).filter_by(active=True).first()
+    if user is None:
+        user = db.session.query(User).first()
+        user.active = True
+        db.session.commit()
     return session.AutoTradersSession(user.token)
 
 

e41d238f2a8bbf7a162da891e7272b20154b0c2d
Ashwin Naren <arihant2math@gmail.com>
2023-11-06 16:13:18 -0800

    gunicorn support

--- gunicorn.py
+++ gunicorn.py
@@ -1,8 +1,9 @@
 from website.app import create_app
 from website.config import BaseConfig
+import os
 
 
-def app(db, secret_key):
-    BaseConfig.SECRET_KEY = secret_key
-    BaseConfig.SQLALCHEMY_DATABASE_URI = db
+def app():
+    BaseConfig.SECRET_KEY = os.environ.get("SECRET_KEY")
+    BaseConfig.SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     return create_app()

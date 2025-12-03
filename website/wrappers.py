ec8fc5361daad4514abecb137c2ba0e67310821f
Ashwin Naren <arihant2math@gmail.com>
2025-12-02 20:40:11 -0800

    fix

--- website/wrappers.py
+++ website/wrappers.py
@@ -1,6 +1,5 @@
 import autotraders.token
 from flask import redirect, url_for, flash, session
-from minify_html import minify
 
 from website.session import get_session, get_user
 
@@ -8,23 +7,8 @@ from website.session import get_session, get_user
 def minify_html(func):
     def wrap(*args, **kwargs):
         result = func(*args, **kwargs)
-        if type(result) is str:
-            return minify(
-                result,
-                do_not_minify_doctype=True,
-                ensure_spec_compliant_unquoted_attribute_values=True,
-                keep_closing_tags=True,
-                keep_comments=False,
-                keep_html_and_head_opening_tags=True,
-                keep_spaces_between_attributes=True,
-                minify_css=True,
-                minify_js=False,
-                remove_bangs=False,
-                remove_processing_instructions=False,
-            )
-        else:
-            print("WARNING: Minifying failed because result was not string")
-            return result
+        # TODO: minify HTML output
+        return result
 
     wrap.__name__ = func.__name__
     wrap.__doc__ = func.__doc__
